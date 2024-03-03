6b49d2f7a787acb2f534a7befcd5b8a279927dbd
Brian H. Castelhano <brian.castelhano@gmail.com>
2024-03-03 17:33:02 -0300

    fixed wirdht cards

--- my-ecommerce/src/Assembly.js
+++ my-ecommerce/src/Assembly.js
@@ -3,14 +3,10 @@ import jsonData from "./data.json"
 import Card from "./Card.js"
 export default function Assembly() {
     const data = jsonData
-    const CardList = ({data})
     return (
         <div className="assembly-div">
             <div className="assembly-nav">
                 <p>All Products</p>
-                <p>T-Shirt</p>
-                <p>Shoes</p>
-                <p>Acessories</p>
             </div>
             <div className="assembly-cards">
                 {data.map(cardData => (
