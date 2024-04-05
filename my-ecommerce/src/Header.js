57c4f6e5734f736f12ddb4ddb6662b3ea2b3887b
Brian H. Castelhano <brian.castelhano@gmail.com>
2024-04-05 20:55:09 -0300

    header height

--- my-ecommerce/src/Header.js
+++ my-ecommerce/src/Header.js
@@ -4,11 +4,7 @@ export default function Header() {
     <nav role="navigation" class="navbar ">
         <i class="fas fa-cart-shopping"></i>
         <div class="content content-fluid">
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-            <a class="navbar-brand"><img /></a>
+        <a class="navbar-brand"><img /></a>
             <div class="icons">
                 <button><i class="bi bi-person-circle"></i> Account </button>
                 <button><i class="bi bi-bag-fill"></i> Shopping </button>
